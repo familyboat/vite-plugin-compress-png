ab8bae3c5c4bca3c488696d1f697749116584c91
familyboat <familyboat@163.com>
2024-07-30 10:17:12 +0800

    执行 deno fmt

--- filter.ts
+++ filter.ts
@@ -1,11 +1,11 @@
-import {glob} from 'glob';
+import { glob } from "glob";
 
 /**
  * 从目标目录中过滤出待压缩的 png 文件
- * 
+ *
  * @param dir 目标目录
  * @param pattern glob pattern
- * 
+ *
  * @returns 待压缩的 png 文件数组
  */
 export async function filter(dir: string, pattern: string | string[]) {
--- filter_test.ts
+++ filter_test.ts
@@ -1,14 +1,14 @@
-import {fromFileUrl, join} from '@std/path';
+import { fromFileUrl, join } from "@std/path";
 import { filter } from "./filter.ts";
-import {assertEquals} from '@std/assert';
+import { assertEquals } from "@std/assert";
 
-Deno.test("compress()", async function() {
-  const testData = new URL('test_data', import.meta.url);
+Deno.test("compress()", async function () {
+  const testData = new URL("test_data", import.meta.url);
   const dir = fromFileUrl(testData);
 
-  const compressed = await filter(dir, '*.png');
+  const compressed = await filter(dir, "*.png");
   const expected = [
-    join(dir, 'dog.png')
-  ]
+    join(dir, "dog.png"),
+  ];
   assertEquals(compressed, expected);
-})
+});
