3e236ddf8622bb2c1bd1f46d6197e39608597b0a
familyboat <84062528+familyboat@users.noreply.github.com>
2024-07-30 10:20:42 +0800

    执行格式化操作 (#19)
    * 最新版的 deno 发布 jsr 包时，会报错，在 publish workflow 中指明 deno 版本
    
    * 执行 deno fmt
    
    ---------
    
    Co-authored-by: familyboat <familyboat@163.com>

--- mod.ts
+++ mod.ts
@@ -1,43 +1,45 @@
 /**
  * @module
- * 
+ *
  * 本模块是 vite 插件，使用 pngquant 工具对 png
  * 格式的图片进行压缩
- * 
+ *
  * @example
  * ```
  * ```
  */
-import { exec } from 'node:child_process';
-import which from 'which';
-import type { Plugin } from 'vite';
+import { exec } from "node:child_process";
+import which from "which";
+import type { Plugin } from "vite";
 import { filter } from "./filter.ts";
 
 export type CompressPngType = {
-  pattern: string | string[]
-}
+  pattern: string | string[];
+};
 
 const defaultOptions: CompressPngType = {
-  pattern: '**/*.png'
-}
+  pattern: "**/*.png",
+};
 
 /**
  * 使用 pngquant 压缩 png 图片
  */
-export default function compressPng (options: CompressPngType = defaultOptions): Plugin {
+export default function compressPng(
+  options: CompressPngType = defaultOptions,
+): Plugin {
   return {
-    name: 'vite-plugin-compress-png',
-     
+    name: "vite-plugin-compress-png",
+
     async writeBundle(output) {
-      const resolvedOrNull = await which('pngquant', { nothrow: true });
+      const resolvedOrNull = await which("pngquant", { nothrow: true });
       if (resolvedOrNull === null) {
-        console.log('未找到图片压缩工具pngquant');
+        console.log("未找到图片压缩工具pngquant");
         return;
       }
 
       const dir = output.dir;
       if (!dir) {
-        return
+        return;
       }
 
       const compressed = await filter(dir, options.pattern);
@@ -46,5 +48,5 @@ export default function compressPng (options: CompressPngType = defaultOptions):
         exec(`pngquant --force --ext .png --quality 80 ${imageFile}`);
       }
     },
-  }
+  };
 }
